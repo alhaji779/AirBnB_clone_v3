0b1cae695f0b0995c83b7541fc93cc8f5ed7f253
Michael Kanu <michaelokanu01@yahoo.com>
2024-05-26 14:31:12 -0700

    api-index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,21 @@
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def showStat():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def getCount():
+    all_dict = {"amenities": 'Amenity',
+                  "cities": 'City',
+                  "places": 'Place',
+                  "reviews": 'Review',
+                  "states": 'State',
+                  "users": 'User'}
+    for key in all_dict.keys():
+        all_dict[key] = storage.count(all_dict.get(key))
+    return jsonify(all_dict)
