9800ec28e5338375179dce3a2c6f013ce078f02e
Udynathcodes <uwahuduakobong@gmail.com>
2024-05-27 13:56:52 +0100

    update file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """ index file
 """
-from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from api.v1.views import app_views
+
 
 @app_views.route('/status', strict_slashes=False)
-def showStat():
+def show_stat():
+    """Returns the status of the API"""
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats')
-def getCount():
-    all_dict = {"amenities": 'Amenity',
-                  "cities": 'City',
-                  "places": 'Place',
-                  "reviews": 'Review',
-                  "states": 'State',
-                  "users": 'User'}
+@app_views.route('/stats', strict_slashes=False)
+def get_count():
+    """Returns the count of all objects by type"""
+    all_dict = {
+        "amenities": 'Amenity',
+        "cities": 'City',
+        "places": 'Place',
+        "reviews": 'Review',
+        "states": 'State',
+        "users": 'User'
+    }
     for key in all_dict.keys():
-        all_dict[key] = storage.count(all_dict.get(key))
+        all_dict[key] = storage.count(all_dict[key])
     return jsonify(all_dict)
